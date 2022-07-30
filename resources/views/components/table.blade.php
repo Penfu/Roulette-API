9b72fe995f711c4fb1a6d4854ef818c1eae17b5a
Armand Marechal <sypenfu@gmail.com>
2022-07-30 17:14:25 +0200

    Add users index view

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,13 @@
+<div class="my-2 align-middle min-w-full overflow-x-auto overflow-hidden rounded sm:rounded-lg shadow-md shadow-gray-300">
+    <table class="min-w-full table-auto">
+        <thead class="bg-gray-800 text-white">
+            <tr>
+                {{ $head }}
+            </tr>
+        </thead>
+
+        <tbody class="bg-white divide-y divide-gray-200">
+            {{ $body }}
+        </tbody>
+    </table>
+</div>
