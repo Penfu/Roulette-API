cefbc98225a932f9f19bdc94f4fb03b9a93a3fcb
Armand Marechal <sypenfu@gmail.com>
2022-11-21 13:05:45 +0100

    Update users index method

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -18,13 +19,21 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Penfu',
             'email' => 'penfu@gmail.com',
+            'email_verified_at' => now(),
+            'balance' => 10000,
             'password' => Hash::make('root'),
+            'remember_token' => Str::random(10),
         ]);
 
         User::create([
             'name' => 'Knouille',
             'email' => 'knouille@gmail.com',
+            'email_verified_at' => now(),
+            'balance' => 5000,
             'password' => Hash::make('root'),
+            'remember_token' => Str::random(10),
         ]);
+
+        User::factory()->count(20)->create();
     }
 }
