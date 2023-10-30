0b5c78706703fd53ef6da38a5b8e7ab542e238e0
Penfu <sypenfu@gmail.com>
2023-10-30 17:12:39 +0100

    Increase balance length

--- database/factories/BetFactory.php
+++ database/factories/BetFactory.php
@@ -12,9 +12,11 @@ class BetFactory extends Factory
 {
     public function definition()
     {
-        $user = User::where('balance', '>=', 1)->inRandomOrder()->first();
+        $user = User::where('balance', '>=', 1)
+            ->inRandomOrder()
+            ->first();
         $roll = Roll::inRandomOrder()->first();
-        $win = $this->faker->boolean(50); // Without, there to much lose as the odds are 1/3
+        $win = $this->faker->boolean(30); // Without, there to much lose as the odds are 1/3
         $colors = new Collection(['red', 'black', 'green']);
         $color = $win ? $roll->color : $this->faker->randomElement($colors->except($roll->color));
         $amount = random_int(1, $user->balance);
@@ -32,7 +34,7 @@ class BetFactory extends Factory
             'user_id' => $user->id,
             'roll_id' => $roll->id,
             'created_at' => $date,
-            'updated_at' => $date
+            'updated_at' => $date,
         ];
     }
 }
