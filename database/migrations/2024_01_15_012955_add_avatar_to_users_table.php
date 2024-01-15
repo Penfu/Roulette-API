0c39f3eae7684987547557e3c6e53882dc3f9a71
Penfu <sypenfu@gmail.com>
2024-01-15 03:27:43 +0100

    Change avatar type to external api reference

--- database/migrations/2024_01_15_012955_add_avatar_to_users_table.php
+++ database/migrations/2024_01_15_012955_add_avatar_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('avatar')->nullable()->after('balance');
+            $table->json('avatar')->nullable();
         });
     }
 
